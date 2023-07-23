ea4ab4d09de4fca031f36885266cb5b143944289
phy-lei <765373325@qq.com>
2023-07-23 22:31:47 +0800

    feat: 发送图片

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,73 +1,131 @@
-'use client'
+'use client';
 
-import axios from 'axios'
-import { FC, useRef, useState } from 'react'
-import { toast } from 'react-hot-toast'
-import TextareaAutosize from 'react-textarea-autosize'
-import Button from './ui/Button'
+import axios from 'axios';
+import { FC, useRef, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import TextareaAutosize from 'react-textarea-autosize';
+import Button from './ui/Button';
 
 interface ChatInputProps {
-  chatPartner: User
-  chatId: string
+  chatPartner: User;
+  chatId: string;
 }
 
 const ChatInput: FC<ChatInputProps> = ({ chatPartner, chatId }) => {
-  const textareaRef = useRef<HTMLTextAreaElement | null>(null)
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [input, setInput] = useState<string>('')
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [input, setInput] = useState<string>('');
 
-  const sendMessage = async () => {
-    if(!input) return
-    setIsLoading(true)
+  const sendMessage = async (value: string) => {
+    if (!value) return;
+    setIsLoading(true);
 
     try {
-      await axios.post('/api/message/send', { text: input, chatId })
-      setInput('')
-      textareaRef.current?.focus()
+      await axios.post('/api/message/send', { text: value, chatId });
+      setInput('');
+      textareaRef.current?.focus();
     } catch {
-      toast.error('Something went wrong. Please try again later.')
+      toast.error('Something went wrong. Please try again later.');
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
+
+  const handlePaste = async (event: any) => {
+    // 剪贴板没数据，则直接返回
+    if (!event.clipboardData || !event.clipboardData.items) {
+      return;
+    }
+    for (let i = 0, len = event.clipboardData.items.length; i < len; i++) {
+      const item = event.clipboardData.items[i];
+      if (item.kind === 'file') {
+        const file = item.getAsFile();
+        if (item.type.match('^image/')) {
+          console.log(
+            '%c [ file ]',
+            'font-size:13px; background:pink; color:#bf2c9f;',
+            file
+          );
+
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = async function (e: any) {
+            const base64 = e.target.result;
+
+            console.log(
+              '%c [ xxx ]',
+              'font-size:13px; background:pink; color:#bf2c9f;',
+              base64
+            );
+            const filePath =
+              new Date().toLocaleDateString().replace(/\//g, '') +
+              '/' +
+              file.name;
+            // const res = await axios.put(
+            //   'https://api.github.com/repos/phy-lei/blob-imgs/contents/' +
+            //     filePath,
+            //   {
+            //     message: 'upload img',
+            //     content: base64,
+            //   },
+            //   {
+            //     headers: {
+            //       Authorization: 'token ' + process.env.GITHUB_ACCESS_TOKEN,
+            //     },
+            //   }
+            // );
+            // console.log('[ res ] >', res);
+            sendMessage(base64);
+          };
+        }
+      }
+    }
+  };
 
   return (
-    <div className='border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0'>
-      <div className='relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600'>
+    <div className="border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+      <div className="relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
         <TextareaAutosize
           ref={textareaRef}
           onKeyDown={(e) => {
             if (e.key === 'Enter' && !e.shiftKey) {
-              e.preventDefault()
-              sendMessage()
+              e.preventDefault();
+              sendMessage(input);
             }
           }}
+          onPaste={(e) => handlePaste(e)}
           rows={1}
+          disabled={isLoading}
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder={`Message ${chatPartner.name}`}
-          className='block w-full resize-none border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:py-1.5 sm:text-sm sm:leading-6'
+          className="block w-full resize-none border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:py-1.5 sm:text-sm sm:leading-6"
         />
 
         <div
           onClick={() => textareaRef.current?.focus()}
-          className='py-2'
-          aria-hidden='true'>
-          <div className='py-px'>
-            <div className='h-9' />
+          className="py-2"
+          aria-hidden="true"
+        >
+          <div className="py-px">
+            <div className="h-9" />
           </div>
         </div>
 
-        <div className='absolute right-0 bottom-0 flex justify-between py-2 pl-3 pr-2'>
-          <div className='flex-shrin-0'>
-            <Button isLoading={isLoading} onClick={sendMessage} type='submit'>
+        <div className="absolute right-0 bottom-0 flex justify-between py-2 pl-3 pr-2">
+          <div className="flex-shrin-0">
+            <Button
+              isLoading={isLoading}
+              onClick={() => sendMessage(input)}
+              type="submit"
+            >
               Post
             </Button>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatInput
+export default ChatInput;
